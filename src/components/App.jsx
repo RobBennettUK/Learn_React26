97d6c931c69b10e317f3de8acf949cfc568f0308
RobBennettUK <38293815+RobBennettUK@users.noreply.github.com>
2020-11-19 09:34:14 +0000

    Part One

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [click, setClick] = useState("");
+
+  function handleChange(event) {
+    // console.log(event.target.value);
+    // console.log(event.target.placeholder);
+    // console.log(event.target.type);
+    setName(event.target.value);
+  }
+
+  function handleClick() {
+    setClick(name);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {click}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
 
 export default App;
+
+// Using a form with React, you should set the value of the form to event.target.value
+// This makes it a controlled component with a single source of truth
+// https://reactjs.org/docs/forms.html#controlled-components
+
+// The default behaviour of a button within a form is for it to refresh
+// It triggers a method on the form called onSubmit
+// This means we could move the onClick={handleClick} into the form as onSubmit={handleClick}
